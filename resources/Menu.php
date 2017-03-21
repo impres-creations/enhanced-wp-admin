12f25dfedd264e371ab2b7d446f4a1b0108991f5
Danny van Holten <danny@impres.nl>
2017-03-21 20:08:00 +0100

    Menu Improvements

--- resources/Menu.php
+++ resources/Menu.php
@@ -5,7 +5,11 @@ namespace EnhancedWPAdmin;
 /**
  * Class EnhancedMenu
  *
- * This class is responsible for removing / editing the meta boxes.
+ * This class is responsible for changing the WP Admin menu.
+ *
+ * @note: this is not a fully fledged menu plug-in. If you want full control over your menu use the provided code
+ * by WordPress or use a plug-in that specifically targets this feature.
+ * For example: https://wordpress.org/plugins/admin-menu-editor/
  */
 class Menu
 {
@@ -21,8 +25,8 @@ class Menu
 	{
 		// Change the menu
 		add_action('wp_before_admin_bar_render', [$this, 'removeAdminBarItems']);
-		add_action('admin_init', [$this, 'addAdminMenuSeperator'], 5);
-		add_action('admin_init', [$this, 'changeAdminMenuItems'], 1);
+		add_action('admin_init', [$this, 'addAdminMenuSeperator'], 11);
+		add_action('admin_init', [$this, 'changeAdminMenuItems'], 11);
 	}
 
 	/**
@@ -32,10 +36,9 @@ class Menu
 	{
 		global $menu;
 
-		unset($menu[4]); // Remove the first seperator
-		unset($menu[59]); // Remove the second seperator so we can reset the count
 
-		$seperators = [49, 59];
+		unset($menu[4],$menu[30], $menu[59], $menu[99]); // Remove the seperators so we can reset the count.
+		$seperators = [44.9, 49.9, 59.9];
 
 		foreach ($seperators as $key => $seperator) {
 			$menu[$seperator] = ['', 'read', 'separator' . $key, '', 'wp-menu-separator'];
@@ -69,30 +72,43 @@ class Menu
 
 		foreach ($menu as $key => $item) {
 			switch ($item[2]) {
+				case 'index.php': // Remove the dashboard but keep the updates.
+					foreach ($submenu['index.php'] as $subItem) {
+						if ($subItem[2] === 'update-core.php') {
+							$submenu['options-general.php'][0.1] = $subItem;
+
+							ksort($submenu['options-general.php']);
+						}
+					}
+					unset($menu[$key]);
+					break;
 				case 'themes.php': // Remove the appearence menu item and add the menus in the main menu
 					foreach ($submenu['themes.php'] as $subItem) {
 						if ($subItem[2] === 'nav-menus.php') {
-							$menu[50] = $subItem;
-							$menu[50][] = '';
-							$menu[50][] = 'menu-top menu-icon-appearance menu-top-first';
-							$menu[50][] = 'menu-appearance';
-							$menu[50][] = 'dashicons-menu';
+							$menu[45] = $subItem;
+							$menu[45][] = '';
+							$menu[45][] = 'menu-top menu-icon-appearance menu-top-first';
+							$menu[45][] = 'menu-appearance';
+							$menu[45][] = 'dashicons-menu';
 						}
 					}
 					unset($menu[$key]);
 					break;
 				case 'upload.php': // Change the position of media
-					$this->adminMenuCount($key, 51);
+					$this->adminMenuCount($key, 46);
 					break;
 				case 'gf_edit_forms': // Change the position of gravityforms
-					$this->adminMenuCount($key, 52);
+					$this->adminMenuCount($key, 47);
+					break;
+				case 'users.php': // Change the position of users
+					$this->adminMenuCount($key, 50);
+					break;
+				case 'wpseo_dashboard': // Change the position of users
+					$this->adminMenuCount($key, 51);
 					break;
 				case 'acf-options': // Change the position of the acf options
-					$this->adminMenuCount($key, 53 + $acfOptions);
+					$this->adminMenuCount($key, 52 + $acfOptions);
 					$acfOptions++;
-					break;
-				case 'users.php': // Change the position of gravityforms
-					$this->adminMenuCount($key, 58);
 					break;
 			}
 		}
